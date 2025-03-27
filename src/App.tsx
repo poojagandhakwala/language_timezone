65e54847b3c8bb904cc733226284335e91015f3c
poojagandhakwala <pnmodi124@gmail.com>
2025-03-27 19:35:20 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import LanguageSelection from "./LanguageSelection.tsx";
 
 function App() {
--- src/LanguageSelection.tsx
+++ src/LanguageSelection.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import moment from "moment-timezone";
+import { Dropdown, Menu } from "antd";
 
-type LanguageProps = {
+interface LanguageProps {
   languages: string[];
-};
+}
 
 const LanguageSelection = ({ languages }: LanguageProps) => {
   const [timezone, setTimezone] = useState<string[]>([]);
@@ -46,55 +47,68 @@ const LanguageSelection = ({ languages }: LanguageProps) => {
     }
   }, [time]);
 
+  const menu = (array: string[], onClick: Function) => (
+    <Menu defaultActiveFirst defaultValue={"Select"}>
+      <Menu.Item className="!text-xl" disabled>
+        Select
+      </Menu.Item>
+      {array &&
+        array.map((item, index) => (
+          <Menu.Item
+            className="!text-xl"
+            key={index}
+            onClick={() => onClick(item)}
+            style={{ width: "100%" }}
+          >
+            {item}
+          </Menu.Item>
+        ))}
+    </Menu>
+  );
+
   return (
-    <div className="bg-[#083f8f] !text-white rounded-xl p-4 px-12 text-center text-xl h-[800px] md:h-[400px] ">
-      <h2 className="text-center text-2xl p-5 font-semibold py-8">
+    <div className="bg-[#083f8f] !text-white rounded-xl p-4 px-8 text-center text-xl h-[800px] md:h-[400px] w-[85%] mx-auto mt-5">
+      <h2 className="text-center text-[1.6rem] p-5 font-semibold py-8">
         Please Select Language and then time zone for getting specific time of
         selected time Zone.
       </h2>
-      <div className="flex flex-col md:flex-row justify-between text-start space-x-4 self-center mt-4">
+      <div className="flex flex-col md:flex-row justify-between text-start space-x-4 self-center mt-6">
         <div className="lg:w-1/2">
           <label className="text-xl font-semibold">Select Language</label>
-          <select
-            className="bg-white text-black rounded w-full mt-2"
-            value={language}
-            onChange={(e) => setLanguage(e.target.value)}
-            defaultValue={""}
+          <Dropdown
+            overlay={menu(languages, setLanguage)}
+            trigger={["click"]}
+            arrow={false}
+            className="bg-white text-black rounded flex p-2 mt-1 cursor-pointer"
           >
-            <option value="">Select Language</option>
-            {languages.map((item, index) => (
-              <option key={index}>{item}</option>
-            ))}
-          </select>
+            {language || "Select Language"}
+          </Dropdown>
         </div>
         <div className="lg:w-1/2">
           <label className="text-xl font-semibold">Select Timezone</label>
-          <select
-            className="bg-white text-black rounded w-full mt-2"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            defaultValue={""}
+          <Dropdown
+            overlay={menu(timezone, setTime)}
+            trigger={["click"]}
+            arrow={false}
+            className="bg-white text-black rounded flex p-2 mt-1 cursor-pointer"
           >
-            <option value="">Select Timezone</option>
-            {timezone.map((item: any, index: number) => (
-              <option key={index}>{item}</option>
-            ))}
-          </select>
+            {time || "Select Timezone"}
+          </Dropdown>
         </div>
       </div>
-      <div className="flex justify-between py-8 space-x-4 text-start font-semibold">
-        {time && (
-          <>
-            <div className="w-1/2 ">
-              <h3 className="text-xl">Timezone Date & Time :</h3>
-              <h3 className="text-xl">Country :</h3>
-            </div>
-            <div className="w-1/2 ">
-              <h3>{dateString}</h3>
-              <h3>{country}</h3>
-            </div>
-          </>
-        )}
+      <div
+        className={`flex justify-between py-8 space-x-4 text-start font-semibold ${
+          !time && "hidden"
+        }`}
+      >
+        <div className="w-1/2 ">
+          <h3 className="text-xl">Timezone Date & Time :</h3>
+          <h3 className="text-xl">Country :</h3>
+        </div>
+        <div className="w-1/2 ">
+          <h3>{dateString}</h3>
+          <h3>{country}</h3>
+        </div>
       </div>
     </div>
   );
